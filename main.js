1c1766d14380058d50570d129080216ff17b7a6c
circuar <sy13500481374@163.com>
2025-04-17 19:02:49 +0800

    update

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import path from 'path';
 import Store from 'electron-store';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
+import Database from 'better-sqlite3';
 
 // 获取当前模块的文件路径
 const __filename = fileURLToPath(import.meta.url);
@@ -76,6 +77,38 @@ const runtimeKVDataBase = new Map();
 const configStore = new Store();
 // 初始化会话索引缓存
 const sessionIndexCache = new Store({ name: 'sessionIndexCache' });
+// 创建数据库
+const db = new Database('data.sqlite', { verbose: console.log });
+
+
+// 初始化保存的用户表
+db.prepare(`CREATE TABLE IF NOT EXISTS connection_user_index (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,   -- 主键
+        uid VARCHAR(10) UNIQUE NOT NULL,        -- UID
+        username VARCHAR(20) NOT NULL,          -- 用户名
+        display_name VARCHAR(20) DEFAULT NULL,  -- 备注名
+        img_path VARCHAR(256),                  -- 头像本地缓存路径
+        last_online_timestamp INTEGER NOT NULL  -- 上次在线时间戳
+    );`).run();
+db.prepare(`CREATE TABLE IF NOT EXISTS connection_group_index (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,   -- 主键
+        gid VARCHAR(20) UNIQUE NOT NULL,        -- GID
+        group_name VARCHAR(20) NOT NULL,        -- 群组名
+        display_name VARCHAR(20) DEFAULT NULL,  -- 备注名
+        img_path VARCHAR(256)                   -- 图片缓存路径
+    );`).run();
+db.prepare(`CREATE TABLE IF NOT EXISTS message_data (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,       -- 主键
+        mid INTEGER UNIQUE NOT NULL,                -- 消息ID
+        affiliated_type INTEGER NOT NULL,           -- 所属类型, 0: user, 1: group
+        from_gid VARCHAR(20) DEFAULT NULL,          -- 如果消息来自于群组, 则保存群组的GID
+        from_uid VARCHAR(10) NOT NULL,              -- 创建消息的UID
+        unread INTEGER DEFAULT 1,                   -- 是否未读, 0: read, 1: unread
+        message_type INTEGER NOT NULL DEFAULT 0,    -- 消息类型, 0: text, 1: img, 2: sound, 3: file
+        message_content VARCHAR(512),               -- 消息内容, 如果是文件则使用路径
+        timestamp INTEGER NOT NULL                  -- 消息服务器接收时间
+    );`).run();
+
 
 // 应用启动时加载登录窗口
 app.whenReady().then(() => {
@@ -109,6 +142,9 @@ app.whenReady().then(() => {
         loginWindow.close();
         createMainWindow();
     });
+
+    // database操作
+    ipcMain.handle('sql', (event, sql) => db.prepare(sql));
 
     createLoginWindow();
 });
--- ui/window/chat/chat.js
+++ ui/window/chat/chat.js
@@ -1,64 +1,89 @@
 const { ipcRenderer } = require('electron');
 
+/*
+ * 渲染进程不维护用户信息的引用，查询使用数据库接口
+ */
+
+
+
+
+// DOM元素======================================================================
 // 全局加载界面
 var windowLoadingUI = document.getElementById('windowLoadingBox');
 // 聊天窗口加载界面
+// TODO: html
 var chatLoadingUI = document.getElementById('')
 
 // 置顶会话div
 var pinnedSessionListDiv = document.getElementById('main-chatListPinned');
 // 置顶会话标题
 var pinnedSessionGroupLabel = document.getElementById('pinnedLabel');
-// 置顶会话元素Map
-var pinnedSessionElemMap = new Map();
+
 
 // 普通会话div
 var commonSessionListDiv = document.getElementById('main-chatListChat');
 // 普通会话标题
 var commonSessionGroupLabel = document.getElementById('commonLabel');
-// 普通会话元素Map
-var commonSessionElemMap = new Map();
+
 
 // 静音会话div
 var muteSessionListDiv = document.getElementById('main-chatListMute');
 // 置顶会话标题
 var muteSessionGroupLabel = document.getElementById('muteLabel');
+
+// 会话组元素Map================================================================
+// 置顶会话元素Map
+var pinnedSessionElemMap = new Map();
+// 普通会话元素Map
+var commonSessionElemMap = new Map();
 // 置顶会话元素Map
 var muteSessionElemMap = new Map();
 
+// Connection元素Map============================================================
+// 群组连接元素Map
+var groupConnectionElemMap = new Map();
+// 用户连接元素Map
+var userConnectionElemMao = new Map();
 
-// 本地缓存会话索引数组，由init()调用ipc赋值，对应本地缓存
-var sessionIndexArray;
+
+
+// 配置=========================================================================
+// 应用properties
+var properties;
 // 服务器地址
 var server;
 // 端口号
 var port;
 // 请求地址
 var baseUrl;
-// 应用properties
-var properties;
-
 // 用户token
 var token;
 
+// 变量=========================================================================
+// 用户额外的实时信息，例如在线状态，额外文本等
+var extraRealtimeUserInfo = new Map();
 
+// 本地会话缓存映射
+var sessionIndexArray;
+
+
+// 脚本执行入口 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 // 显示加载界面
 windowLoadingUI.style.display = 'block';
 
 // 推迟加载，避免复杂任务导致窗口未完全显示就卡死
 setTimeout(() => {
     init().then(() => {
+        console.log('OK');
         windowLoadingUI.style.display = 'none';
 
     });
 }, 1000);
+// 脚本执行入口 END >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
-
-
+// function define =============================================================
 async function init() {
-    // 显示加载界面
-    sessionIndexArray = await ipcRenderer.invoke('getSessionIndexArray');
 
     // 获取properties对象
     properties = await ipcRenderer.invoke('getProperties');
@@ -70,109 +95,336 @@ async function init() {
     // 获取token
     token = await ipcRenderer.invoke('getDataFromRuntimeDB', 'token');
 
-    // 刷新本地会话缓存
-    refreshSessionIndexCache();
+    // 读取本地会话缓存到渲染进程
+    sessionIndexArray = await readSessionIndexCache();
 
+    await renderSessionList();
 
 }
+
+// 获取sessionIndex的缓存数组
+async function readSessionIndexCache() {
+    return await ipcRenderer.invoke('getSessionIndexArray');
+}
+
+// 传输sessionArray到主进程，主进程调用set()直接覆盖缓存数组
+async function saveSessionIndexCache(sessionArray) {
+    ipcRenderer.invoke('setSessionIndexArray', sessionArray);
+}
+
+// 加载本地缓存的sessionList到页面
+async function renderSessionList() {
+    const group = [pinnedSessionElemMap, commonSessionElemMap, muteSessionElemMap];
+
+    for (const sessionMetaData of sessionIndexArray) {
+
+
+        // sessionMetaData:
+        // this.sessionIdRef = sessionIdRef;
+        // this.sessionRefType = sessionRefType;
+        // this.sessionGroup = sessionGroup;
+        // this.name = name;
+
+
+
+        let sessionIdRef = sessionMetaData.sessionIdRef;
+        let sessionRefType = sessionMetaData.sessionRefType;
+        let sessionGroup = sessionMetaData.sessionGroup;
+        let name = sessionMetaData.name;
+
+
+
+        let imgPath;
+        let messageInfo;
+
+        let message;
+        let time;
+        let count;
+
+
+        if (sessionRefType == 0) {
+            imgPath = (await ipcRenderer.invoke('sql', 'SELECT img_path FROM connection_user_index WHERE uid = ?;')).get(sessionIdRef);
+            messageInfo = (await ipcRenderer.invoke('sql', `SELECT message_type, message_content, timestamp
+                                                                FROM message_data
+                                                                WHERE affiliated_type = 0
+                                                                AND from_uid = ?
+                                                                AND timestamp = (
+                                                                    SELECT MAX(timestamp)
+                                                                    FROM message_data
+                                                                    WHERE affiliated_type = 0
+                                                                    AND from_uid = ?
+                                                                );
+                                                            `)).get(sessionIdRef);
+            count = (await ipcRenderer.invoke('sql', `SELECT COUNT(1)
+                                                      FROM message_data
+                                                      WHERE affiliated_type = 0
+                                                      AND from_uid = ?
+                                                      AND unread = 1;
+                                                      `)).get(sessionIdRef);
+        } else {
+            imgPath = (await ipcRenderer.invoke('sql', 'SELECT img_path FROM connection_group_index WHERE gid = ?;')).get(sessionIdRef);
+            messageInfo = (await ipcRenderer.invoke('sql', `SELECT message_type, message_content, timestamp
+                                                                FROM message_data
+                                                                WHERE affiliated_type = 1
+                                                                AND from_gid = ?
+                                                                AND timestamp = (
+                                                                    SELECT MAX(timestamp)
+                                                                    FROM message_data
+                                                                    WHERE affiliated_type = 1
+                                                                    AND from_gid = ?
+                                                                );
+                                                            `)).get(sessionIdRef);
+            count = (await ipcRenderer.invoke('sql', `SELECT COUNT(1)
+                                                      FROM message_data
+                                                      WHERE affiliated_type = 1
+                                                      AND from_gid = ?
+                                                      AND unread = 1;
+                                                      `)).get(sessionIdRef);
+        }
+
+        if (messageInfo.message_type == 0) {
+            message = '󱧎 ' + messageInfo.message_content;
+        } else if (messageInfo.message_type == 1) {
+            message = '[图片]';
+        } else if (messageInfo.message_type == 2) {
+            message = '[语音]';
+        } else {
+            message = '[文件]';
+        }
+
+        time = convertTimestampToTagTime(messageInfo.timestamp * 1000);
+
+        let uiNode = new UISessionListNode(sessionIdRef, sessionRefType, sessionGroup, false, imgPath, null, name, null, message, time, count)
+        group[sessionMetaData.sessionGroup].set(sessionMetaData.sessionIdRef, uiNode);
+    }
+
+}
+
+
+// 仅做ui处理，不处理逻辑
+class UISessionListNode {
+    constructor(sessionIdRef, sessionRefType, group, focus, imgPath, flag, name, extraText, message, time, count) {
+
+        this.sessionIdRef = sessionIdRef;      // 会话保留的群组或用户的ID
+        this.sessionRefType = sessionRefType;
+        this.elemRef = null;
+        this.group = group;
+        this.focus = focus;
+        this.imgPath = imgPath;
+        this.flag = flag;
+        this.name = name;
+        this.extraText = extraText;
+        this.message = message;
+        this.time = time;
+        this.count = count;
+
+        this.render();
+    }
+
+    render() {
+        if (this.elemRef) {
+            this.elemRef.remove();
+        }
+
+        let focusDisplayControl = this.focus ? '' : 'style="display: none;"';
+        let imgSrc = this.imgPath ? this.imgPath : '';
+        let flagDisplayControl = this.flag ? '' : 'style="display: none;"';
+        let extraTextDisplayControl = this.extraText ? '' : 'style="display: none;"';
+        let countBubbleDisplayControl = this.count && this.count > 0 ? '' : 'style="display: none;"';
+
+        // 创建UI节点
+        this.elemRef = document.createElement('div');
+        // 设置类名
+        this.elemRef.className = 'chatItem';
+        this.elemRef.innerHTML =
+            `
+            <div class="identifier" ${focusDisplayControl}></div>
+            <div class="chatListUserImgFallback">
+                <img src="${imgSrc}">
+                </div>
+
+            <div class="chatItemText">
+                <div class="topGroup">
+                    <span class="userFlag" ${flagDisplayControl} >${this.flag}</span>
+                    <span class="userShowName">${this.name}</span>
+                    <span class="extraText" ${extraTextDisplayControl}>${this.extraText}</span>
+                </div>
+                <div class="messageText">
+                    ${this.message}
+                </div>
+            </div>
+            <div class="chatItemWidget">
+                <div class="time">${this.time}</div>
+                <div class="count" ${countBubbleDisplayControl}>${this.count}</div>
+            </div>
+        `;
+
+        if (this.group == 0) {
+            this.elemRef = pinnedSessionGroupLabel.insertAdjacentElement('afterend', this.elemRef);
+        } else if (this.group == 1) {
+            this.elemRef = commonSessionGroupLabel.insertAdjacentElement('afterend', this.elemRef);
+        } else if (this.group == 2) {
+            this.elemRef = muteSessionGroupLabel.insertAdjacentElement('afterend', this.elemRef);
+        }
+    }
+
+    // 组移动
+    resetGroup(groupIndex) {
+        if (groupIndex == this.group)
+            return;
+
+        let groups = [pinnedSessionElemMap, commonSessionElemMap, muteSessionElemMap];
+        let groupListLabels = [pinnedSessionGroupLabel, commonSessionGroupLabel, muteSessionGroupLabel];
+
+        //原组移除
+        groups[this.group].delete(this.sessionIdRef);
+        //加入新组
+        groups[groupIndex].set(this.sessionIdRef, this);
+        // 更新属性
+        this.group = groupIndex;
+        // ui更新
+        groupListLabels[groupIndex].insertAdjacentElement('afterend', this.elemRef);
+    }
+    // 焦点设置
+    setFocus() {
+        // 显示焦点指示器
+        if (!this.focus) {
+            this.elemRef.getElementsByClassName('identifier')[0].style.display = '';
+            this.focus = true;
+        }
+    }
+    // 取消焦点
+    cancelFocus() {
+        if (this.focus) {
+            this.elemRef.getElementsByClassName('identifier')[0].style.display = 'none';
+            this.focus = false;
+        }
+    }
+    // 设置图片
+    setImagePath(path) {
+        this.imgPath = path;
+        this.elemRef.querySelectorAll('.chatListUserImgFallback')[0].querySelectorAll('img')[0].src = path;
+    }
+    // 设置name
+    setName(name) {
+        this.name = name;
+        // 更新UI
+        this.elemRef.querySelectorAll('.chatItemText')[0].querySelectorAll('.topGroup')[0].querySelectorAll('')
+    }
+
+}
+
+
+class SessionIndex {
+    constructor(sessionIdRef, sessionRefType, sessionGroup, name) {
+        this.sessionIdRef = sessionIdRef;
+        this.sessionRefType = sessionRefType;
+        this.sessionGroup = sessionGroup;
+        this.name = name;
+    }
+}
+
+
+function convertTimestampToTagTime(timestamp) {
+    let time = new Date(timestamp);
+    let current = new Date();
+
+    if (current.getTime() - timestamp > 24 * 60 * 60 * 1000 && current.getTime() - timestamp <= 2 * 24 * 60 * 60 * 1000) {
+        return '昨天';
+    } else if (current.getTime() - timestamp > 2 * 24 * 60 * 60 * 1000 && current.getTime() - timestamp <= 3 * 24 * 60 * 60 * 1000) {
+        return '前天';
+    } else if (current.getTime() - timestamp > 3 * 24 * 60 * 60 * 1000 && current.getTime() - timestamp <= 365 * 24 * 60 * 60 * 1000) {
+        let monthNum = current.getMonth();
+        let monthStr = '' + monthNum;
+        if (monthNum < 10)
+            monthStr = '0' + monthNum;
+
+        let dayNum = current.getDay();
+        let dayStr = '' + dayNum;
+        if (dayNum < 10)
+            dayStr = '0' + dayNum;
+
+        return monthStr + '-' + dayStr;
+    } else if (current.getTime() - timestamp >= 0 && current.getTime() - timestamp <= 24 * 60 * 60 * 1000) {
+        let tail;
+        let hoursNum = current.getHours();
+        let hoursStr = '' + hoursNum;
+
+        if (hoursNum > 12) {
+            hoursNum = hoursNum - 12;
+            tail = 'PM';
+        } else {
+            tail = 'AM';
+        }
+
+        if (hoursNum < 10) {
+            hoursStr = '0' + hoursNum;
+        }
+
+        let secNum = current.getSeconds();
+        let secStr = '' + secNum;
+        if (dayNum < 10)
+            dayStr = '0' + secNum;
+
+        return hoursStr + ':' + secStr + ' ' + tail;
+    } else {
+        return '很久'
+    }
+}
+
+async function downloadUserList(callback) {
+    let uid = await ipcRenderer.invoke('getDataFromRuntimeDB', 'uid');
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', baseUrl + '/api/user/' + uid + '/userlist');
+    xhr.setRequestHeader('content-type', 'application/json');
+    let req = {
+        token: token
+    }
+
+    xhr.onload = async () => {
+        if (this.status == 200) {
+            resp = JSON.parse(xhr.responseText);
+
+            // 清空旧数据
+            (await ipcRenderer.invoke('sql', 'DELETE FROM connections_user_index;')).run();
+            // 重置主键
+            (await ipcRenderer.invoke('sql', 'DELETE FROM sqlite_sequence WHERE name = connections_user_index;')).run();
+
+            for (let user of resp) {
+                (await ipcRenderer.invoke('sql', 'INSERT INTO connections_user_index VALUES (null, ?, ?, ?, ?, ?)')).run();
+            }
+        }
+        callback();
+
+    }
+
+    xhr.send(req);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 function closeCurrentWindow() {
     ipcRenderer.invoke('closeCurrentWindow');
 }
 
 
-function saveSessionIndexArray() {
-    ipcRenderer.invoke('setSessionIndexArray', sessionIndexArray);
-}
-
-
-
-// 注意单一职责，此处只渲染数据，不做内容存储及其他校验
-function renderSessionListItem(sessionItem, focus) {
-    let focusDisplay = focus ? '' : 'style="display: none;"';
-    let imgSrc = sessionItem.imgPath ? sessionItem.imgPath : '../../resources/img/userhead.jpg';
-    let flagDiv = sessionItem.userFlag ? '<span class="userFlag">' + sessionItem.userFlag + '</span>' : '';
-    let extraText = sessionItem.extraText ? '< span class="extraText" >' + sessionItem.extraText + '</span>' : '';
-    let countBubble = sessionItem.count && count > 0 ? '<div class="count">' + count + '</div>' : '';
-
-    let elemStr = `
-        <div class="chatItem">
-            <div class="identifier" ${focusDisplay}></div>
-            <div class="chatListUserImgFallback">
-                <img src="${imgSrc}" />
-            </div>
-
-            <div class="chatItemText">
-                <div class="topGroup">
-                    ${flagDiv}
-                    <span class="userShowName">${item.sessionName}</span>
-                    ${extraText}
-                </div>
-                <div class="messageText">
-                    ${item.messageText}
-                </div>
-            </div>
-            <div class="chatItemWidget">
-                <div class="time">${item.time}</div>
-                ${countBubble}
-            </div>
-        </div>
-    `
-    var item;
-    if (group == 0) {
-        // 末尾插入，获取dom对象
-        item = pinnedSessionListDiv.insertAdjacentHTML('beforeend', elemStr);
-        pinnedSessionListDiv.insertBefore(item, pinnedSessionGroupLabel.nextSibling)
-
-    } else if (group == 1) {
-        item = commonSessionListDiv.insertAdjacentHTML('beforeend', elemStr);
-        commonSessionListDiv.insertBefore(item, commonSessionGroupLabel.nextSibling)
-
-    } else if (group == 2) {
-        item = muteSessionListDiv.insertAdjacentHTML('beforeend', elemStr);
-        muteSessionListDiv.insertBefore(item, muteSessionGroupLabel.nextSibling)
-    }
-
-    return item;
-}
-
-// 同步额外的实时信息到本地缓存
-function refreshSessionIndexCache() {
-
-}
-
-
-// 加载本地缓存的sessionList到页面
-function loadSessionList() {
-    
-}
-
-
-function loadFriendList() {
-
-}
-
-function loadSessionList() {
-
-}
-
-function refreshFriendList() {
-
-}
 
 function ShowChatWindowLoadingUI() {
 
 }
 
 function HideChatWindowLoadingUI() {
-
-}
-
-
-function showChatWindow() {
-
-}
-
-function hideChatWindow() {
 
 }
